8fd6564c5fec10add13e3e3be0425ebd5f4df276
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2014-12-28 20:19:09 +0100

    Documenting the PipingBag class

--- src/Di/PipingBag.php
+++ src/Di/PipingBag.php
@@ -10,14 +10,27 @@ use Ray\Di\Injector;
 
 class PipingBag {
 
+/**
+ * The current injector instance
+ *
+ * @var Injector
+ */
 	protected static $_instance;
 
-	public static function create(array $modules = [], $cacheConfig = 'default') {
+/**
+ * Creates a new injector instance
+ *
+ * @param array|callable $modules A list of modules to be installed. Or a callable
+ * that will return the list of modules.
+ * @param string $cacheConfig The name of the cache config to use.
+ * @return Injector
+ */
+	public static function create($modules = [], $cacheConfig = 'default') {
 		$cacheConfig;
 		$cache = $cacheConfig ? new CacheAdapter($cacheConfig) : null;
 
 		if (is_callable($modules)) {
-			$modules = $modules();
+			$modules = (array)$modules();
 		}
 
 		$modules = array_map(function($module) {
@@ -43,6 +56,12 @@ class PipingBag {
 		return $injector;
 	}
 
+/**
+ * Get/Set the Injector instance.
+ *
+ * @param Injector $instance The injector to be used.
+ * @return Injector
+ */
 	public static function container(Injector $instance = null) {
 		if ($instance !== null) {
 			static::$_instance = $instance;
@@ -50,10 +69,23 @@ class PipingBag {
 		return static::$_instance;
 	}
 
+/**
+ * Return an instance of a class after resolving its dependencies.
+ *
+ * @param $class The class name or interface name to load.
+ * @return mixed
+ */
 	public static function get($class) {
 		return static::container()->getInstance($class);
 	}
 
+/**
+ * Return an instance of a class by a name after resolving its dependencies.
+ *
+ * @param $class The class name or interface name to load.
+ * @param $name The alias given to this class for namespacing the configuration.
+ * @return mixed
+ */
 	public static function getNamed($class, $name = AbstractModule::NAME_UNSPECIFIED) {
 		return static::container()->getNamedInstance($class, $name);
 	}
