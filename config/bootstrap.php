acc56536cc503c1186627895eabee53578026ce3
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-03-30 13:46:19 +0200

    Missing container injection

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -14,6 +14,7 @@ AnnotationReader::addGlobalIgnoredName('triggers');
 $instance = Cache::remember('pipingbag.instance', function () use ($modules) {
     return PipingBag::create($modules);
 }, $cache);
+PipingBag::container($instance);
 
 DispatcherFactory::add(
     $instance->getInstance('PipingBag\Routing\Filter\ControllerFactoryFilter')
